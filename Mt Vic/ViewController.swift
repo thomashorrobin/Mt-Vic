4baae93e428aaf4ec2f9e10404e1bf2280be048e
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2018-05-22 17:56:37 +1200

    connected timer to stop/start buttons

--- Mt Vic/ViewController.swift
+++ Mt Vic/ViewController.swift
@@ -13,10 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateTimerLable()
-        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
-            self.seconds = self.seconds + 1
-            self.updateTimerLable()
-        })
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -40,11 +36,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var stopButton: UIButton!
     
     @IBAction func startTimer(_ sender: UIButton) {
+        seconds = 0
+        self.updateTimerLable()
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
+            self.seconds = self.seconds + 1
+            self.updateTimerLable()
+        })
         stopButton.isEnabled = true
         startButton.isEnabled = false
     }
     
     @IBAction func stopTimer(_ sender: UIButton) {
+        timer.invalidate()
         stopButton.isEnabled = false
         startButton.isEnabled = true
     }
