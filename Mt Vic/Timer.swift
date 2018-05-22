1a2fc180a484699d3d54ac30c96684da85e64a70
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2018-05-22 17:10:50 +1200

    added important function

--- Mt Vic/Timer.swift
+++ Mt Vic/Timer.swift
@@ -7,3 +7,10 @@
 //
 
 import Foundation
+
+func timeString(time:TimeInterval) -> String {
+    let hours = Int(time) / 3600
+    let minutes = Int(time) / 60 % 60
+    let seconds = Int(time) % 60
+    return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
+}
